02a70b6d89e91d15020438f3fe06fcfdad3e933e
LeonardoSilva <apx88.0@gmail.com>
2021-01-27 17:24:03 -0500

    created website lol

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .nav li a").click(function () {
+    // applying again smooth scroll on nav items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle nav script
+  $(".nav-btn").click(function () {
+    $(".navbar .nav").toggleClass("active");
+    $(".nav-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "Freelancer", "Athlete"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Freelancer", "Athlete"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
